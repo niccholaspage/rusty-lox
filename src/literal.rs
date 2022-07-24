25470032c520ffd85f7768538d65592004a151db
Nicholas Nassar <niccholaspage@gmail.com>
2022-07-24 16:57:32 -0400

    Fix printing of number literals

--- src/literal.rs
+++ src/literal.rs
@@ -9,7 +9,10 @@ pub enum Literal {
 impl Display for Literal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
-            Literal::Number(num) => num.fmt(f),
+            // Using the Display implementation of f64 causes numbers like
+            // 5.0 to be printed as 5, causing tests to fail. The Debug implementation
+            // doesn't do this, so we use that instead.
+            Literal::Number(num) => write!(f, "{num:?}"),
             Literal::String(string) => string.fmt(f),
             Literal::Nil => write!(f, "null"),
         }
